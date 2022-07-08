1b64fdbbb0814ce800896786878e79e447692b1c
Christian Dike <chreestiandike@gmail.com>
2022-07-08 04:15:01 +0100

    Add Exercises 1.3 to 1.5
    modify variable definitions of App into a single javascript object and
    refactor code in other parts of the application to render correctly.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,35 +1,34 @@
 const App = () => {
-  const course = "Half stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        part3={part3}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercise1={exercises1}
-        exercise2={exercises2}
-        exercise3={exercises3}
-      />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
 
 const Header = (props) => (
   <>
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   </>
 );
 
@@ -37,23 +36,35 @@ const Part = (props) => (
   <>
     <p>
       {" "}
-      {props.part} {props.exercises}{" "}
+      {props.name} {props.exercises}{" "}
     </p>
   </>
 );
 
 const Content = (props) => (
   <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    <Part
+      name={props.course.parts[0].name}
+      exercises={props.course.parts[0].exercises}
+    />
+    <Part
+      name={props.course.parts[1].name}
+      exercises={props.course.parts[1].exercises}
+    />
+    <Part
+      name={props.course.parts[2].name}
+      exercises={props.course.parts[2].exercises}
+    />
   </>
 );
 
 const Total = (props) => (
   <>
     <p>
-      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+      Number of exercises{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
     </p>
   </>
 );
